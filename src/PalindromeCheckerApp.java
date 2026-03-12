8856aaec96288429817f85aca3c1df9af137cfcf
prashStep <prashbordoloi6@gmail.com>
2026-03-12 14:43:12 +0530

    UC1: entry point to the application

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,17 @@
+
+public class PalindromeCheckerApp {
+
+    /**
+     * Main method - Entry point of the application.
+     * JVM starts execution from here.
+     */
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println("        Palindrome Checker App      ");
+        System.out.println("        Version : 1.0               ");
+        System.out.println("====================================");
+
+        System.out.println("Application started successfully!");
+    }
+}
